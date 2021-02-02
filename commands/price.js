663e2c77769a57b9fbc23757f3f53370352d4eef
Youssef El Saadany <youssef.saadany@gmail.com>
2021-02-02 23:31:00 +0100

    feat: checks if source & destination currencies are available
    if not, sends an error message

--- commands/price.js
+++ commands/price.js
@@ -4,18 +4,44 @@ const axios = require("axios");
 module.exports = {
   name: "price",
   description: "check cryptocurrency price on coingecko",
-  execute(message, args) {
+  async execute(message, args) {
     if (args.length === 0) {
       message.channel.send("Parameters are missing...");
     } else {
       let currency = "usd";
+      var current_coin = args[0];
       if (args.length === 2) {
         currency = args[1];
       }
+      await axios
+        .get("https://api.coingecko.com/api/v3/coins/list")
+        .then((res) => {
+          let found = false;
+          res.data.map((coin) => {
+            if (
+              current_coin === coin["id"] ||
+              current_coin === coin["symbol"]
+            ) {
+              current_coin = coin["id"];
+              found = true;
+            }
+          });
+          if (!found) {
+            message.channel.send("This cryptocurrency is not available.");
+          }
+        });
+      console.log(current_coin);
+
       axios
-        .get("https://api.coingecko.com/api/v3/coins/" + args[0])
+        .get("https://api.coingecko.com/api/v3/coins/" + current_coin)
         .then((res) => {
           console.log(res);
+          if (
+            res.data["market_data"]["current_price"][currency] === undefined
+          ) {
+            message.channel.send("Destination currency does not exist.");
+            return;
+          }
           const exampleEmbed = new Discord.MessageEmbed()
             .setColor(
               res.data["market_data"]["price_change_24h"] > 0
@@ -29,7 +55,7 @@ module.exports = {
                 " " +
                 currency.toUpperCase(),
               res.data["image"]["small"],
-              "https://www.coingecko.com/en/coins/" + args[0]
+              "https://www.coingecko.com/en/coins/" + current_coin
             )
             .setDescription(
               "[More info...](https://www.coingecko.com/en/coins/" +
