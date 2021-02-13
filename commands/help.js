0e46ed3725b8a088d8c3052ced442023e9839c57
Youssef El Saadany <youssef.saadany@gmail.com>
2021-02-13 12:39:19 +0100

    feat: updated $help command

--- commands/help.js
+++ commands/help.js
@@ -16,6 +16,22 @@ module.exports = {
           `${prefix}price <ticker> [currency]`,
           "get the current price of a ticker"
         )
+        .addField(
+          `${prefix}convert <amount> <source> <destination>`,
+          "get the current exchange rate for a trading pair"
+        )
+        .addField(
+          `${prefix}balance <ticker>`,
+          "REGISTERED USERS: get your account's balance for a ticker"
+        )
+        .addField(
+          `${prefix}buy|sell <quantity> <destination> <source> <price>`,
+          "REGISTERED USERS: place a limit order to buy/sell <quantity> amount of <destination> with <source> at <price>"
+        )
+        .addField(
+          `${prefix}cancel <order_id>`,
+          "REGISTERED USERS: cancel an order"
+        )
         .setFooter("Made with <3")
     );
   },
