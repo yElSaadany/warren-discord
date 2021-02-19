9cc3afbbe68dcca7badac41f35899a45bc2b74d5
Youssef El Saadany <youssef.saadany@gmail.com>
2021-02-19 23:40:20 +0100

    feat: updated 'quote' command to choose your speaker
    only some quotes of Buffett & Saylor now

--- commands/quote.js
+++ commands/quote.js
@@ -5,11 +5,17 @@ module.exports = {
   name: "quote",
   description: "stream in voice channel or write a quote of Warren Buffett",
   execute(message, client, args) {
+    const speaker = args[0]
+      ? args[0]
+      : ["buffett", "saylor"][Math.floor(Math.random() * 2)];
+    console.log(speaker);
     client.channels.fetch("781218691366781011").then(async (vc) => {
       const voiceConn = await vc.join();
-      const audioQuotes = fs.readdirSync(process.env.ASSETS_PATH + "/audio");
+      const audioQuotes = fs.readdirSync(
+        process.env.ASSETS_PATH + `/audio/${speaker}`
+      );
       voiceConn.play(
-        `${process.env.ASSETS_PATH}/audio/${
+        `${process.env.ASSETS_PATH}/audio/${speaker}/${
           audioQuotes[Math.floor(Math.random() * audioQuotes.length)]
         }`
       );
